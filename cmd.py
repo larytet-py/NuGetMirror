c0b594fcfa6339cdc6c33488cda5b4c14797ebfe
arkady <>
2021-06-11 18:44:32 +0300

    Update

--- cmd.py
+++ cmd.py
@@ -8,10 +8,35 @@ from concurrent.futures import ThreadPoolExecutor, wait
 logger = logging.getLogger()
 
 def process_resource(resource):
-    return resource.get("@id", None)
+    id = resource.get("@id", None)
+    if id is None:
+        logger.error(f"{resource} is missing ID")
+        return None
+
+    try:
+        r = requests.get(id)
+        index = r.json()
+    except Exception:
+        logger.exception(f"Failed to fetch {id}")
+        return None
+
+    if "data" not in index:
+        logger.error(f"'data' is missing in {resource}/{index}")
+        return None
+
+    packages = {}
+    for d in data:
+        package_id = data.get("id", None)
+        if package_id is None:
+            logger.error(f"{resource}/{index}/{data} is missing ID")
+            continue
+        versions = {version.get("@id", None) for version in d.get("versions", [])}
+        packages[package_id] = versions
+
+    return packages
 
 @easyargs
-def main(command, index_url='https://api.nuget.org/v3/index.json', threads=16):
+def main(command, index_url='https://api.nuget.org/v3/index.json', max_threads=16):
     commands = ["hash", "list"]
     if command not in commands:
         logger.error(f"{command} is not from {commands}")
@@ -24,7 +49,7 @@ def main(command, index_url='https://api.nuget.org/v3/index.json', threads=16):
         return -2
 
     resources = index["resources"]
-    with ThreadPoolExecutor(max_workers = 5) as executor:
+    with ThreadPoolExecutor(max_workers = max_threads) as executor:
         for resource in resources:
             process_resource(resource)
 
@@ -35,7 +60,6 @@ def main(command, index_url='https://api.nuget.org/v3/index.json', threads=16):
         for future in futures:
             result = future.result()
             if result is None:
-                logger.error(f"{resource} is missing ID")
                 continue
 
             results.add(result)
